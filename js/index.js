d1b3d30f60b30eabdbf4de9db1b92dd8fc64a914
Ssslakter <67190162+Ssslakter@users.noreply.github.com>
2022-11-24 22:29:24 +0700

    feat: Сделал переходы на страницы при смене url

--- js/index.js
+++ js/index.js
@@ -1,8 +1,27 @@
 $(document).ready(function () {
+  AddNavigationListeners();
   //Load main page
-  $("main").load("/html/main.html");
+  LoadPage(location.pathname);
 });
 
 function LoadPage(url) {
-  $("main").load(url);
+  $("main").empty();
+  if (url == "/") {
+    $("main").load(`/html/main.html`);
+    return;
+  }
+  var name = url.replaceAll("/", "");
+  $("main").load(`/html/${name}.html`);
+}
+
+function AddNavigationListeners() {
+  var all = $("a");
+  for (const link of all) {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      var url = $(e.target).attr("href");
+      history.pushState(null, null, url);
+      LoadPage(location.pathname);
+    });
+  }
 }
