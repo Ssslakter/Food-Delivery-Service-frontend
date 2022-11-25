808ed1d3428a9ff0383e4ef6431847e77be91206
Ssslakter <67190162+Ssslakter@users.noreply.github.com>
2022-11-25 23:09:05 +0700

    feat: Добавил верстку для фильтров Поменял запросы под новую схему

--- js/main.js
+++ js/main.js
@@ -1,29 +1,24 @@
-$(document).ready(function () {
+export function initMain() {
   //Load page consts
-  $("#dishes-list").load("/html/templates.html #card-template");
-
   LoadDishes();
-});
+}
 
 function LoadDishes() {
   $("#dishes-list").empty();
-  fetch(`${ApiURL}/dish`)
-    .then((response) => {
-      return response.json();
-    })
-    .then((json) => {
-      let template = $("#card-template");
-      for (const dish of json.dishes) {
-        let block = template.clone();
-        console.log(dish.id);
-        block.attr("data-id", dish.id);
-        block.find(".dish-title").text(dish.name);
-        block.find(".dish-category").text(`Категория блюда - ${dish.category}`);
-        block.find(".dish-image").attr("src", dish.image);
-        block.find(".dish-description").text(dish.description);
-        block.find(".dish-price").text(`Цена - ${dish.price} р.`);
-        block.removeClass("d-none");
-        $("#dishes-list").append(block);
-      }
-    });
+  $("#dishes-list").load("/html/templates.html #card-template");
+  Get("/dish").then((json) => {
+    let template = $("#card-template");
+    for (const dish of json.dishes) {
+      let block = template.clone();
+      console.log(dish.id);
+      block.attr("data-id", dish.id);
+      block.find(".dish-title").text(dish.name);
+      block.find(".dish-category").text(`Категория блюда - ${dish.category}`);
+      block.find(".dish-image").attr("src", dish.image);
+      block.find(".dish-description").text(dish.description);
+      block.find(".dish-price").text(`Цена - ${dish.price} р.`);
+      block.removeClass("d-none");
+      $("#dishes-list").append(block);
+    }
+  });
 }
