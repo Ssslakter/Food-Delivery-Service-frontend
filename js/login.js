bccfe4d80ae2c149f4332aeaaeaa81b3149a3ec1
Ssslakter <67190162+Ssslakter@users.noreply.github.com>
2022-11-25 23:09:23 +0700

    fix: Поменял запросы логина под новую схему

--- js/login.js
+++ js/login.js
@@ -1,21 +1,35 @@
-window.onload = function () {
-  LoadPage(location.pathname);
-};
+import { LoadPage } from "/js/index.js"
 
-$("#login").click(() => {
-  var data = $('form').serializeArray();
-  data = ToJsObject(data);
-  userEmail = data.email
-  Post(`${ApiURL}/account/login`, data)
-    .then((response) => {
-      userToken = response.token
-      let loginText = $("#user-login");
-      loginText.html(userEmail)
-      loginText.removeClass("d-none");
-      $("#exit").html("Выйти");
-      LoadPage('/');
-      console.log(userToken)
-    }).catch(
-      e => console.error(e)
-    )
-});
+export function initLogin() {
+  LoadLoginPage();
+  AddLoginListener();
+}
+
+function LoadLoginPage() {
+  let loginText = $("#user-login")
+  loginText.html("Регистрация")
+  loginText.attr("href", "/registration")
+  loginText.removeClass("d-none");
+}
+
+function AddLoginListener() {
+  $("#login").click(() => {
+    console.log("fdlfkdl");
+    var data = $('form').serializeArray();
+    data = ToJsObject(data);
+    userEmail = data.email
+    Post(`/account/login`, data)
+      .then((response) => {
+        userToken = response.token
+        let loginText = $("#user-login");
+        loginText.html(userEmail)
+        loginText.removeClass("d-none");
+        $("#exit").html("Выйти");
+        LoadPage('/');
+        console.log(userToken)
+      }).catch(
+        e => console.error(e)
+      )
+  });
+}
+
