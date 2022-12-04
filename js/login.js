de5f654998cdeda848fa055cadfee8bae52c9d4a
Ssslakter <67190162+Ssslakter@users.noreply.github.com>
2022-12-04 20:44:16 +0700

    feat: Валидация в логине

--- js/login.js
+++ js/login.js
@@ -7,24 +7,40 @@ export function initLogin() {
 
 async function AddLoginListener() {
   $("#login").click(() => {
+    $("#wrong-login").addClass("d-none")
     var data = $('form').serializeArray();
     data = ToJsObject(data);
     var userEmail = data.email
-    Post(`/account/login`, data).then(async (resp) => {
-      if (resp.ok) {
-        let response = await resp.json()
-        window.localStorage.setItem('userToken', response.token)
-        window.localStorage.setItem('userEmail', userEmail);
-        PageLoader.loadPage('/');
-        console.log(response.token)
-      }
-    }).catch(
-      e => console.error(e)
-    )
+    if (ValidateForm()) {
+      Post(`/account/login`, data).then(async (resp) => {
+        if (resp.ok) {
+          let response = await resp.json()
+          window.localStorage.setItem('userToken', response.token)
+          window.localStorage.setItem('userEmail', userEmail);
+          PageLoader.loadPage('/');
+          console.log(response.token)
+        }
+        else if (resp.status == 400) {
+          $("#wrong-login").removeClass("d-none")
+        }
+      })
+    }
   });
 }
 
 export function Logout() {
   localStorage.clear();
   PageLoader.loadPage('/');
+}
+
+
+function ValidateForm() {
+  var form = $("form")
+  form.addClass('was-validated')
+  form.validate({
+    errorPlacement: function (error, element) {
+      return true
+    }
+  })
+  return form.valid();
 }
